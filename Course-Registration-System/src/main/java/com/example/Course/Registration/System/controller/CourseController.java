d20d7129a4864abf0c915c2ef7acee71fdb6662c
DharaneeshCG <dharaneesh1813@gmail.com>
2026-01-04 00:11:40 +0530

    Initialized Backend Code

---
+++ Course-Registration-System/src/main/java/com/example/Course/Registration/System/controller/CourseController.java
@@ -0,0 +1,33 @@
+package com.example.Course.Registration.System.controller;
+
+import com.example.Course.Registration.System.model.Course;
+import com.example.Course.Registration.System.model.CourseRegistry;
+import com.example.Course.Registration.System.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5500")
+public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+    @GetMapping("courses")
+    public List<Course> availableCourses(){
+        return courseService.availableCourses();
+    }
+    @GetMapping("courses/enrolled")
+    public List<CourseRegistry> enrolledCourses(){
+        return courseService.enrolledCourses();
+    }
+
+    @PostMapping("courses/register")
+    public String enrollCourse(@RequestParam("name") String name,
+                               @RequestParam("email") String email,
+                               @RequestParam("courseName") String courseName){
+         courseService.enrollCourse(name, email, courseName);
+         return "Congrats " + name + ", you have enrolled for " + courseName;
+    }
+}
